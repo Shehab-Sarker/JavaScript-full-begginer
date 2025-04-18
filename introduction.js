8f2a3f6dd171a404c5abf6c1f413917568f96f0c
Shehab218 <sarkershehab123@gmail.com>
2025-04-18 19:25:03 +0600

    first commit

---
+++ introduction.js
@@ -0,0 +1,15 @@
+//  JavaScript is one of the 3 languages all web developers must learn:
+//     1. HTML to define the content of web pages
+//     2. CSS to specify the layout of web pages
+//     3. JavaScript to program the behavior of web pages
+
+// What is JavaScript?
+// JavaScript is the programming language of the web.
+// It can update and change both HTML and CSS.
+// It can calculate, manipulate and validate data.
+// JavaScript Can Change HTML Content
+// One of many JavaScript HTML methods is getElementById()
+// The example below "finds" an HTML element (with id="demo"), and changes the element content (innerHTML) to "Hello JavaScript"
+
+// JavaScript Can Change HTML Attribute Values
+// In this example JavaScript changes the value of the src (source) attribute of an <img> tag:
---
+++ js_where_to.js
@@ -0,0 +1,3 @@
+function myfunction1(){
+    document.getElementById("demo2").innerHTML='External scripts are practical when the same code is used in many different web pages.';
+}
