793b51e9b7bc44e9466bc303d7d249645a1cd108
Shehab218 <sarkershehab123@gmail.com>
2025-04-19 15:34:10 +0600

    JS OUTPUT

---
+++ js_output.js
@@ -0,0 +1,14 @@
+// JavaScript Display Possibilities
+// JavaScript can "display" data in different ways:
+
+// Writing into an HTML element, using innerHTML or innerText.
+// Writing into the HTML output using document.write().
+// Writing into an alert box, using window.alert().
+// Writing into the browser console, using console.log().
+
+console.log("Helllo world")
+
+// JavaScript Print
+// JavaScript does not have any print object or print methods.
+// You cannot access output devices from JavaScript.
+// The only exception is that you can call the window.print() method in the browser to print the content of the current window.
